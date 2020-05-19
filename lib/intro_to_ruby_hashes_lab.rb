a35889b1b3298b6e2cc5dd13945ddc9ba8f35bed
eager-class-5814 <candacehchoe@gmail.com>
2020-05-19 00:36:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-if hash["key"]
- hash["key"] += 1
+if hash[key]
+ hash[key] += 1
 else
- puts "Key not found!"
+ hash[key] = 1 
 end
 
 end
